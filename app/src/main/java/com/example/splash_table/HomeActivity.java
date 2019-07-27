542f8f7a015fa35de295f14aedd75a929b6c7ba4
Merci13 <mrjorxe@gmail.com>
2019-07-27 16:23:19 -0600

    ventana Pizza
    se agrego la ventana de pizza y se trato de hacer el evento que lo llama

--- app/src/main/java/com/example/splash_table/HomeActivity.java
+++ app/src/main/java/com/example/splash_table/HomeActivity.java
@@ -2,13 +2,52 @@ package com.example.splash_table;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public String[] tiqueteAvion= new String[5];
+    public List tiquete_de_avion = new ArrayList();
+    /*
+     * Nombre
+     * Apellido
+     * Cedula
+     * Hora de salida
+     * lugar de destino
+     * */
+    public List pizza = new ArrayList();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    public void onClickPizzaVentana(View v){
+        Intent intent = new Intent(this,PizzaTable);
+        startActivity(intent);
+
+
+
+    }
+
+
+    /**
+    * Crear una nueva vista con una serie de inputs para que el usuario llene el pedido de pizza
+     * debe tener
+     * Nombre, Apellido, Direccion, numero de slices y mostrar el precio segun el tamanno de la
+     * pizza
+     * ademas, tiene que mostrarle una serie de sabores a escojer, ya se
+     * Suprema, Jamon y hongos o Hauawana
+    */
+
+
 }
